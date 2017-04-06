b20a3f020973cc950031017669ed8a4484718200
Siddhi Pai <siddhi.pai@gmail.com>
2017-04-06 11:00:43 -0700

    Add customer microservice

---
+++ repositories/CustomerRepository.ts
@@ -0,0 +1,16 @@
+const DataSource = require('../../loopback-datasource-juggler').DataSource;
+
+
+let ds = new DataSource('CustomerDB', {
+    "connector": "memory",
+    "file": "./models/data.json"
+});
+
+var Customer = ds.define('Customer', require('../models/customer.json'));
+
+export class CustomerRepository {
+  async find(): Promise<any> {
+    let customers = await Customer.find();
+    return customers;
+  }
+}
