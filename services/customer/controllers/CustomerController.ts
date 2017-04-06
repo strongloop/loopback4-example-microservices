e9089430ff323c5e1a36f6953ff3a6c228733f30
Siddhi Pai <siddhi.pai@gmail.com>
2017-04-06 14:38:58 -0700

    Review Changes for Customer Microservice

--- services/customer/controllers/CustomerController.ts
+++ services/customer/controllers/CustomerController.ts
@@ -4,9 +4,17 @@ import {CustomerRepository} from '../repositories/customer';
 
 @api(def)
 export class CustomerController {
+<<<<<<< feef5f52490ce3899aa0489eb185ee5f63d124ca
   async getCustomers(): Promise<any> {
     const repository = new CustomerRepository();
     const customers = await repository.find();
     return Promise.resolve(JSON.stringify(customers));
   }
+=======
+    async getCustomers():Promise<any>{
+       const repository = new CustomerRepository();
+       const customers = await repository.find();
+       return Promise.resolve(JSON.stringify(customers));
+    }
+>>>>>>> Review Changes for Customer Microservice
 }
