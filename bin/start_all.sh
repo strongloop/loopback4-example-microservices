4f956688779d1e1719abeeb407972263dc0dded0
Mahesh Patsute <mahesh_patsute@yahoo.com>
2017-04-10 13:10:24 -0700

    adding more scripts

--- bin/start_all.sh
+++ bin/start_all.sh
@@ -23,6 +23,8 @@ npm link loopback-next
 ts-node index.ts &
 sleep 5
 echo "Started all services!!"
-
-echo "Making account summary call to facade"
-curl http://localhost:3000/accountsummary?accountNumber=CHK52321122 | jq
+echo " "
+echo "To run the app, execute: "
+echo "./bin/get-account-summary.sh"
+echo "./bin/get-account.sh"
+echo " "
