e9a4b5b03fcee3b2abd576c6f6ce3bade02e6652
Taranveer Virk <taranveer@virk.cc>
2017-08-16 21:29:24 -0400

    Update transaction microservice

--- services/transaction/index.ts
+++ services/transaction/index.ts
@@ -1,11 +1,38 @@
 import { Application } from '@loopback/core';
 import { TransactionController } from './controllers/TransactionController';
 
-const app = new Application();
+class TransactionApplication extends Application {
+  private _startTime: Date;
 
-app.controller(TransactionController);
-app.bind('http.port').to(3003);
+  constructor() {
+    super();
+    const app = this;
+    app.controller(TransactionController);
+    app.bind('http.port').to(3003);
+  }
 
-app.start();
+  async start() {
+    this._startTime = new Date();
+    return super.start();
+  }
 
-console.log('Application started on port:', app.getSync('http.port'));
+  async info() {
+    const port: Number = await this.get('http.port');
+
+    return {
+      uptime: Date.now() - this._startTime.getTime(),
+      url: 'http://127.0.0.1:' + port,
+    };
+  }
+}
+
+async function main(): Promise<void> {
+  const app = new TransactionApplication();
+  await app.start();
+  console.log('Application Info:', await app.info());
+}
+
+main().catch(err => {
+  console.log('Cannot start the app.', err);
+  process.exit(1);
+});
--- services/transaction/repositories/transaction/index.ts
+++ services/transaction/repositories/transaction/index.ts
@@ -1,23 +1,18 @@
-import juggler = require('loopback-datasource-juggler');
+import { juggler, DataSourceConstructor } from '@loopback/repository';
 const modelDefinition = require('./models/transaction/model-definition.json');
 
 export class TransactionRepository {
-  model: PersistedModel;
+  model;
 
   constructor() {
-    const DataSource = juggler.DataSource;
-    const ds = new DataSource('local-fs', {
+    const ds = new DataSourceConstructor('local-fs', {
       connector: 'memory',
       file: './repositories/transaction/datasources/local-fs/data.json'
     });
-    this.model = ds.define('Transaction', modelDefinition);
+    this.model = ds.createModel('Transaction', modelDefinition);
   }
 
   async find(id): Promise<any> {
     return await this.model.find({ where: { accountNo: id } });
   }
-}
-
-interface PersistedModel {
-  find: Function
 }
