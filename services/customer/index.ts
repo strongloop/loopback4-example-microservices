1afcf1f227f594e8b0794710c8866b7a1f6c345c
Taranveer Virk <taranveer@virk.cc>
2017-08-16 21:12:17 -0400

    Updated customer micro service

--- services/customer/index.ts
+++ services/customer/index.ts
@@ -1,11 +1,38 @@
 import { Application } from '@loopback/core';
 import { CustomerController } from './controllers/CustomerController';
 
-const app = new Application();
+class CustomerApplication extends Application {
+  private _startTime: Date;
 
-app.controller(CustomerController);
-app.bind('http.port').to(3002);
+  constructor() {
+    super();
+    const app = this;
+    app.controller(CustomerController);
+    app.bind('http.port').to(3002);
+  }
 
-app.start();
+  async start() {
+    this._startTime = new Date();
+    return super.start();
+  }
 
-console.log('Application started on port:', app.getSync('http.port'));
+  async info() {
+    const port: Number = await this.get('http.port');
+
+    return {
+      uptime: Date.now() - this._startTime.getTime(),
+      url: 'http://127.0.0.1:' + port,
+    };
+  }
+}
+
+async function main(): Promise<void> {
+  const app = new CustomerApplication();
+  await app.start();
+  console.log('Application Info:', await app.info());
+}
+
+main().catch(err => {
+  console.log('Cannot start the app.', err);
+  process.exit(1);
+});
