f8fae9f46c69c97cbbbc700fa251a03f46246f3d
Taranveer Virk <taranveer@virk.cc>
2017-08-17 20:20:56 -0400

    fix: Add cnstr so no-juggler returns data

--- services/account-without-juggler/repositories/account/models/Account.ts
+++ services/account-without-juggler/repositories/account/models/Account.ts
@@ -1,6 +1,6 @@
-import { model } from '@loopback/repository/';
 import {
   Entity,
+  model,
   ModelDefinition,
   PropertyDefinition
 } from '@loopback/repository';
@@ -9,4 +9,19 @@ import {
 export class Account extends Entity {
   static definition = new ModelDefinition('Account', require('./account/model-definition').properties);
   static modelName = 'Account';
+
+  id: string;
+  customerNumber: string;
+  balance: number;
+  branch: string;
+  type: string;
+  avgBalance: number;
+  minimumBalance: number;
+
+  constructor(body?: Partial<Account>) {
+    super();
+    if (body) {
+      Object.assign(this, body);
+    }
+  }
 }
