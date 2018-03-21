8278c1a4bdfa958e96cbc000544a841b69555f95
Thinus Naude <thinusn@gmail.com>
2018-03-21 16:42:34 +0200

    refactor: use proper types where available

--- services/account/src/controllers/account.controller.ts
+++ services/account/src/controllers/account.controller.ts
@@ -2,19 +2,23 @@ import {api} from '@loopback/rest';
 import {def} from './account.controller.api';
 import {AccountRepository} from '../repositories';
 import {repository} from '@loopback/repository';
+import {Account} from '../models';
+import {Filter, Where} from '@loopback/repository/src/query';
 
 @api(def)
 export class AccountController {
-  constructor(
-    @repository('AccountRepository') private repo: AccountRepository,
-  ) {}
-
-  async getAccount(id: string) {
-    return await this.repo.findById(id);
+  constructor(@repository('AccountRepository') private accountRepository: AccountRepository,) {
   }
 
-  async getAccounts(filter: string) {
-    return await this.repo.find(JSON.parse(filter));
+  async getAccount(id: string): Promise<Account> {
+    return await this.accountRepository.findById(id);
+  }
+
+  async getAccounts(filter?: Filter | string): Promise<Account[]> {
+    if (typeof filter === 'string') {
+      filter = JSON.parse(filter) as Filter;
+    }
+    return await this.accountRepository.find(filter);
   }
 
   async createAccount(accountInstance: {
@@ -22,15 +26,25 @@ export class AccountController {
     balance: number;
     branch: string;
     type: string;
-  }) {
-    return await this.repo.create(accountInstance);
+  }): Promise<Account> {
+    return await this.accountRepository.create(accountInstance);
   }
 
-  async updateAccount(where = '', data: {balance: number}) {
-    return await this.repo.updateAll(data, JSON.parse(where));
+  async updateAccount(where: Where, data: { balance: number }) {
+    if (typeof  where === 'string') {
+      where = JSON.parse(where) as Where;
+    }
+    if (typeof  data === 'string') {
+      data = JSON.parse(data);
+    }
+
+    return await this.accountRepository.updateAll(data, where);
   }
 
-  async deleteAccount(where: string) {
-    return await this.repo.deleteAll(JSON.parse(where));
+  async deleteAccount(where: Where) {
+    if (typeof  where === 'string') {
+      where = JSON.parse(where) as Where;
+    }
+    return await this.accountRepository.deleteAll(where);
   }
 }
--- services/account/src/models/account.model.ts
+++ services/account/src/models/account.model.ts
@@ -1,5 +1,5 @@
 import {Entity, model} from '@loopback/repository';
-import {accountDefinition} from './';
+import {accountDefinition} from './account.definition';
 
 @model(accountDefinition)
 export class Account extends Entity {}
