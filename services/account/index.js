94453d5e0157911a7c6aad0175d96d260d274307
Thinus Naude <thinusn@gmail.com>
2018-03-22 18:42:20 +0200

    refactor(account): add typescript export and main runner

--- services/account/index.js
+++
@@ -1,5 +0,0 @@
-const application = (module.exports = require('./dist/src'));
-
-if (require.main === module) {
-  application.main();
-}
--- services/account/index.ts
+++ services/account/index.ts
@@ -1,1 +1,10 @@
+import * as application from './src';
+
 export * from './src';
+
+if (require.main === module) {
+  application.main().catch(err => {
+    console.log('Cannot start the app.', err);
+    process.exit(1);
+  });
+}
