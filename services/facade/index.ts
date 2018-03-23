c28f69262bdaa0e5235543035d087e126a9c4673
Thinus Naude <thinusn@gmail.com>
2018-03-23 12:22:02 +0200

    refactor(facade): add index.ts

--- services/facade/index.ts
+++ services/facade/index.ts
@@ -1,46 +1,10 @@
-import {Application, ApplicationConfig} from '@loopback/core';
-import {AccountController} from './controllers/AccountManagementController';
-import {RestBindings, RestComponent, RestServer} from '@loopback/rest';
+import * as application from './src';
 
-class FacadeMicroservice extends Application {
-  private _startTime: Date;
+export * from './src';
 
-  constructor(options?: ApplicationConfig) {
-    options = Object.assign(
-      {},
-      {
-        components: [RestComponent],
-      },
-      options,
-    );
-    super(options);
-    const app = this;
-    app.controller(AccountController);
-  }
-
-  async start() {
-    this._startTime = new Date();
-    return super.start();
-  }
-
-  async info() {
-    const rest = await this.getServer(RestServer);
-    const port: Number = await rest.get(RestBindings.PORT);
-    return {
-      appName: 'facade',
-      uptime: Date.now() - this._startTime.getTime(),
-      url: `http://127.0.0.1:${port}`,
-    };
-  }
+if (require.main === module) {
+  application.main().catch(err => {
+    console.log('Cannot start the app.', err);
+    process.exit(1);
+  });
 }
-
-async function main(): Promise<void> {
-  const app = new FacadeMicroservice();
-  await app.start();
-  console.log('Application Info:', await app.info());
-}
-
-main().catch(err => {
-  console.log('Cannot start the app.', err);
-  process.exit(1);
-});
